d30c9613232032670b7a34dfec5a1ed479c01d1c
Arsh <mdrakib01730653037@gmail.com>
2025-05-30 16:13:48 +0600

    Update db_connect.js

--- db/db_connect.js
+++ db/db_connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Db_connect = async () => {
   try {
-    await mongoose.connect(process.env.Mongo_uri);
+    await mongoose.connect("mongodb+srv://rakib:99880@cluster0.kaond.mongodb.net/puccypuccy");
     console.log("database connected !!");
   } catch (error) {
     console.log("database failed !!");
